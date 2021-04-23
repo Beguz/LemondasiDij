7aa8c27e47d19c6abf3ef936f962a070c77a5c1c
Beguz <82580483+Beguz@users.noreply.github.com>
2021-04-23 20:25:34 +0200

    Update Szallas.java

--- Szallas.java
+++ Szallas.java
@@ -1,44 +1,44 @@
 package lemondasidij;
-/*
-import java.time.DayOfWeek;
-import java.time.LocalDateTime;
-*/
+
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeConstants;
 
 public class Szallas {
+
     private DateTime erkezes;
-    
-    public Szallas() {}
+
+    public Szallas() {
+    }
+
     public Szallas(DateTime erkezes) {
         DateTime uj = erkezes.withHourOfDay(14).withMinuteOfHour(0);
-        this.erkezes=uj;
+        this.erkezes = uj;
     }
+
     public int getVisszajaroSzazalek(DateTime mikor) {
-        int counter=0;
+        int szamlalo = 0;
         DateTime temp = mikor;
-        while(temp.isBefore(this.erkezes.minusDays(1))){
-        if(temp.getDayOfWeek()==DateTimeConstants.SUNDAY||temp.getDayOfWeek()==DateTimeConstants.SUNDAY) {
-            counter++;
-        }
-            temp=temp.plusDays(1);
-        }
-        
-        if(mikor.plusDays(counter).isBefore(this.erkezes.minusDays(1))){
-        if (mikor.plusDays(counter).isBefore(this.erkezes.minusDays(3))) {
-            if (mikor.isBefore(this.erkezes.minusMonths(1))){
-                return 100;
-            } else {
-                return 90;
+        while (temp.isBefore(this.erkezes.minusDays(1))) {
+            if (temp.getDayOfWeek() == DateTimeConstants.SUNDAY || temp.getDayOfWeek() == DateTimeConstants.SATURDAY) {
+                szamlalo++;
             }
-        } else {
-            return 50;
+            temp = temp.plusDays(1);
         }
-            
+
+        if (mikor.plusDays(szamlalo).isBefore(this.erkezes.minusDays(1))) {
+            if (mikor.plusDays(szamlalo).isBefore(this.erkezes.minusDays(3))) {
+                if (mikor.isBefore(this.erkezes.minusMonths(1))) {
+                    return 100;
+                } else {
+                    return 90;
+                }
+            } else {
+                return 50;
+            }
+
         } else {
             return 0;
         }
-       
-        
+
     }
 }
